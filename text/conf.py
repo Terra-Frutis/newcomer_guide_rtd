0d178ae9413d702c3301fc7ce66d200fc3af00d3
Argonllama <argonllama@gmail.com>
2024-08-27 15:18:36 -0400

    Update conf.py

--- text/conf.py
+++ text/conf.py
@@ -3,4 +3,28 @@ extensions = [
     "myst_parser",
     'sphinx_rtd_theme',
 ]
+latex_elements = {
+# The paper size ('letterpaper' or 'a4paper').
+'papersize': 'letterpaper',
+
+# The font size ('10pt', '11pt' or '12pt').
+'pointsize': '12pt',
+
+'fontpkg': r"""
+\PassOptionsToPackage{bookmarksnumbered}{hyperref}
+
+""",
+
+# Additional stuff for the LaTeX preamble.
+'preamble': r"""
+\usepackage{setspace}
+""",
+
+'footer': r"""
+""",
+
+'maketitle': r'''
+\pagenumbering{arabic}
+''',
+}
 html_theme = "sphinx_rtd_theme"
