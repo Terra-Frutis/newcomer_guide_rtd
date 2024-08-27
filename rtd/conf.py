fb2658f194bcb91465460bb0a84804c1a5fd8fc1
Argonllama <argonllama@gmail.com>
2024-08-27 16:39:56 -0400

    Update conf.py

--- rtd/conf.py
+++ rtd/conf.py
@@ -1,1 +1,21 @@
 master_doc = '../text/index'
+html_theme = 'epub'
+html_theme_options = {
+"footer": "false",
+    
+}
+latex_elements = {
+# The paper size ('letterpaper' or 'a4paper').
+'papersize': 'letterpaper',
+'classoptions':',openany',
+# The font size ('10pt', '11pt' or '12pt').
+'pointsize': '12pt',
+
+'fontpkg': r"""
+\PassOptionsToPackage{bookmarksnumbered}{hyperref}
+""",
+'maketitle': r'''
+\pagenumbering{arabic}
+''',
+'extraclassoptions': 'openany,oneside'    
+}
